04360a5208610f9420e4e444321726a789efa14c
Zagir <azazoha@gmail.com>
2025-12-02 11:31:30 +0400

    update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/bash
+URL="$(cat data/url.txt)"
 
-curl --write-out "%{http_code}\n" "https://localhost:8000/" --output output.txt --silent
+curl --write-out "%{http_code}\n" $URL --output output.txt --silent
